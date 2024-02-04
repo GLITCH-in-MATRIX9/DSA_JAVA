8e46646cec38468cd221be0f8fb89f6ffbc1790c
Anjali Dass <98489478+GLITCH-in-MATRIX9@users.noreply.github.com>
2024-02-04 23:33:12 +0530

    commit

---
+++ java_basics/day1_q2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class day1_q2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        for(int i=0;i<=number;i++){
+            System.out.println(i);
+
+        }
+
+        
+
+
+    }
+}
