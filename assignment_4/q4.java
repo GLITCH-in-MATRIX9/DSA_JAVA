61bdc9bf9acb65656c8689213e2980607e37706d
Anjali Dass <98489478+GLITCH-in-MATRIX9@users.noreply.github.com>
2024-02-11 23:02:56 +0530

    update

---
+++ assignment_4/q4.java
@@ -0,0 +1,16 @@
+
+// sum of all even numbers from 1 to 100 using for and continue
+import java.util.Scanner;
+
+public class q4 {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 != 0) {
+                continue;
+            }
+            sum+=i;
+        }System.out.println("the sum is "+sum);
+    }
+}
