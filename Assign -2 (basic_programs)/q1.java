66aebd33c47fbfd07aadede6d85ab09b94834c68
Anjali Dass <98489478+GLITCH-in-MATRIX9@users.noreply.github.com>
2024-02-19 18:52:24 +0530

    update

---
+++ Assign -2 (basic_programs)/q1.java
@@ -0,0 +1,11 @@
+//declaring primitive datatypes
+public class q1 {
+    public static void main(String[] args) {
+        int a = 2;
+        float b = 12.23f;
+        double c = 123.234;
+        char d = 'A';
+        boolean e = true;
+
+    }
+}
