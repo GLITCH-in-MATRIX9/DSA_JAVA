33c81c4f020c1761e99eec5bb7332f722c3c8bee
Anjali Dass <98489478+GLITCH-in-MATRIX9@users.noreply.github.com>
2024-02-21 23:00:01 +0530

    update

--- Assignment-6 (Array)/array_3.java
+++ Assignment-6 (Array)/array_3.java
@@ -1,17 +1,25 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class array_3 {
-    int print2largest(int arr[], int n) {
+    public static int print2largest(int arr[], int n) {
+        Arrays.sort(arr);
+        for (int i =n-1; i >= 1; i--) {
+            if (arr[i-1] != arr[i]) {
+                return arr[i-1];
 
-        return n;
-       
+            }
+
+        }
+        return -1;
+
     }
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        
-        
-
-
+        Scanner sc = new Scanner(System.in);
+        int array[] = { 10, 5, 10 };
+        int ans = print2largest(array, array.length);
+        System.out.println("The second largest element is " + ans);
 
     }
 
