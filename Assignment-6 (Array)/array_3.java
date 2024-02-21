34242f5ac3d89f5ac220d3065e3935ee1f679aca
Anjali Dass <98489478+GLITCH-in-MATRIX9@users.noreply.github.com>
2024-02-21 21:18:23 +0530

    update

---
+++ Assignment-6 (Array)/array_3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class array_3 {
+    int print2largest(int arr[], int n) {
+
+        return n;
+       
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        
+        
+
+
+
+    }
+
+}
