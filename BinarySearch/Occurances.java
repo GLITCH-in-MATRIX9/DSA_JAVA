bb96e4925b54d2ccdd3ecb94c1efe5a1ad8132ae
Anjali Dass <98489478+GLITCH-in-MATRIX9@users.noreply.github.com>
2024-06-28 00:16:47 +0530

    28th june

---
+++ BinarySearch/Occurances.java
@@ -0,0 +1,10 @@
+package BinarySearch;
+
+public class Occurances {
+    public static void main(String[] args) {
+        int n = 7;
+        int target = 13;
+        int[] v = {3,4,13,13,13,20,40};
+    }
+    
+}
